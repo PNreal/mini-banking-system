43f5463caab9b6dde5ecfd540deb4839be363956
PNreal <pnreal.hust@gmail.com>
2025-11-30 22:18:50 +0700

    Initial commit - Mini Banking System

---
+++ services/notification-service/notification-service/src/test/java/com/minibank/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minibank.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
